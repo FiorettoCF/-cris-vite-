6016cf1247db87bf8af81e659bb04cd68169a209
Fioretto Cigarro Francisco <FiorettoCF@s2.rpn.ch>
2017-12-07 14:47:59 +0100

    add random system

---
+++ ecris-vite/ecris-vite/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecris_vite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int rndNumber;
+
+            string[] secretWord =
+                        {
+               "bill gates", "rock", "voix", "lady", "anti", "rare", "da", "salut"
+            };
+
+            /*aléatoire*/
+            rndNumber = rand.Next(0, secretWord.Length);
+            /*aléatoire*/
+            string motAleatoire = secretWord[rndNumber];
+            Console.WriteLine("{0}", motAleatoire);
+
+
+
+
+            Console.Write("\nPresser une touche pour terminer le programme ... ");
+            Console.ReadKey();
+       
+        }
+    }
+}
